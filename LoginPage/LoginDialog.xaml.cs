4dac9857cac75dc2eb773abbc7408aa50be2236b
Fran-Y <121924314+Fran-Y@users.noreply.github.com>
2023-07-11 15:08:57 -0400

    Add files via upload

---
+++ LoginPage/LoginDialog.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UnitTestLoginPage;
+
+namespace BookStoreGUI
+{
+    /// <summary>
+    /// LoginDialog.xaml
+    /// </summary>
+    public partial class LoginDialog : Window
+    {
+        public LoginDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+           
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+
+        }
+    }
+}
---
+++ LoginPage/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿/* ************************************************************
+ * For students to work on assignments and project.
+ * Permission required material. Contact: xyuan@uwindsor.ca 
+ * ************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using UnitTestLoginPage;
+
+namespace BookStoreGUI
+{
+    /// Interaction logic for MainWindow.xaml
+    public partial class MainWindow : Window
+    {
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var userData = new UserData();
+            var dlg = new LoginDialog();
+            dlg.Owner = this;
+            dlg.ShowDialog();
+            // Process data entered by user if dialog box is accepted
+            if (dlg.DialogResult == true)
+            {
+                if (userData.LogIn(dlg.nameTextBox.Text, dlg.passwordTextBox.Password) == true)
+                    this.statusTextBlock.Text = "You are logged in as User #" + userData.UserID;
+                else
+                    MessageBox.Show("You could not be verified. Please try again.");
+            }
+        }
+        private void exitButton_Click(object sender, RoutedEventArgs e) { this.Close(); }
+        public MainWindow() { InitializeComponent(); }
+        private void Window_Loaded(object sender, RoutedEventArgs e) { }
+        private void addButton_Click(object sender, RoutedEventArgs e) { }
+        private void chechoutButton_Click(object sender, RoutedEventArgs e) { }
+    }
+}
